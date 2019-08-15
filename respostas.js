cd3a902b7113551fc144378ec3692df9beef245f
GabrieliFrança <gabifrancat.2002@gmail.com>
2019-08-15 16:14:50 -0400

    AtividadeGabi

--- respostas.js
+++ respostas.js
@@ -1,22 +1,19 @@
 // 1. Mostrar a idade dos usuarios
-const { data } = require("./data"); 
+const data = require("./data"); 
 const novoAge = data.map(data => `${data.age}`);
 console.table(novoAge);
 
 //2 Mostrar mulheres com idade maior que 18
-const { data } = require("./data");
-const genderFemale = data.filter( data.gender.female>=18);
+const genderFemale = data.filter(data => data.gender.includes('female') && data.age >=18);
+console.log(genderFemale);
 
 //3 Procurar um usuario da cidade Monroe
-const { data } = require("./data");
-var found = data.find(data.city===Monroe); 
+var found = data.find(data => data.city==="monroe"); 
 console.table(found);
 
 //4 Idade maior que 50
-const { data } = require("./data");
-const vetor;
-for (vetor = 0; vetor < data.length; vetor++) {
-const idade= vetor*2;
+for (var x = 0; x <= data.length; x++) {
+const idade= data.age[x]*2;
 if (idade<=50)
 {
   console.table(idade);
